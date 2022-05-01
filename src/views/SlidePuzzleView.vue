c320eb14ab3aa9275cae972a8fadf1bbad8759a9
naoki <naokipgt@gmail.com>
2022-05-01 15:46:11 +0900

    slide-puzzle : view 作成

---
+++ src/views/SlidePuzzleView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    slide puzzle
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'SlidePuzzleView',
+  created() {
+    console.log('hoge')
+  },
+})
+</script>
