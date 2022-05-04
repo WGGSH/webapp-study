51d1693be30f5ee51c53e961c3cda1f0446e2afa
naoki <naokipgt@gmail.com>
2022-05-04 10:17:07 +0900

    feat (lifegame) : ライフゲームページ作成

---
+++ src/components/life-game/life-game-field.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <div>
+    aaa
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'life-game-field',
+})
+
+</script>
---
+++ src/views/LifeGameView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div>life game</div>
+    <LifeGameFiled></LifeGameFiled>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import LifeGameFiled from '../components/life-game/life-game-field.vue'
+
+export default defineComponent({
+  name: 'LifeGameView',
+  components: {
+    LifeGameFiled,
+  },
+})
+</script>
