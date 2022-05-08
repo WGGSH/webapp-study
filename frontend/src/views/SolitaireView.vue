aed254843e19a135d898e2a4a8089d63563fa11b
naoki <naokipgt@gmail.com>
2022-05-08 18:42:46 +0900

    feat (solitiaire) : ソリティアの view 作成

---
+++ frontend/src/views/SolitaireView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div>solitaire</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'SolitaireView',
+})
+</script>
